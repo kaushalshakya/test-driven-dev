95947947fa819db0755f6563c08e2e8ef70debb8
kaushalshakya <kshakya101@gmail.com>
2024-03-29 15:02:39 +0545

    login test cases

--- src/services/auth/login.service.ts
+++ src/services/auth/login.service.ts
@@ -1,10 +1,19 @@
+import { checkEmail } from "../../helpers";
 import { APIResponse } from "../../types/interfaces/response.interface";
 import { UserLogin } from "../../types/interfaces/user.interface";
 
 export const loginService = async (
   credentials: UserLogin
 ): Promise<APIResponse> => {
-  const { email, password } = credentials;
+  if (!credentials.email || !credentials.password) {
+    return { status: 400, message: "Enter your credentials" };
+  }
 
-  return { status: 400, message: "" };
+  const emailTest = checkEmail(credentials.email);
+
+  if (!emailTest) {
+    return { status: 400, message: "Invalid email address" };
+  }
+
+  return { status: 200, message: "" };
 };
--- src/tests/auth/login.test.ts
+++ src/tests/auth/login.test.ts
@@ -11,7 +11,29 @@ describe("Login User", () => {
       const loginDetails = {};
 
       const response = await loginService(loginDetails);
+      expect(response.status).toBe(400);
+    });
+
+    test("should return 400 when email is invalid", async () => {
+      const loginDetails = {
+        email: "test123",
+        password: "asdaw",
+      };
+
+      const response = await loginService(loginDetails);
+      expect(response.status).toBe(400);
+    });
+
+    test("successful login", async () => {
+      const loginDetails = {
+        email: "test.user@jest.com",
+        password: "test123",
+      };
+
+      const response = await loginService(loginDetails);
       expect(response.status).toBe(200);
+      expect(response.data).toHaveProperty("accessToken");
+      expect(response.data).toHaveProperty("refreshToken");
     });
   });
 });
