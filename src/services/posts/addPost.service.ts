79f9c511eceb114b64b75c5ab9ecccb602610296
kaushalshakya <kshakya101@gmail.com>
2024-04-02 15:35:23 +0545

    test cases

--- src/services/posts/addPost.service.ts
+++ src/services/posts/addPost.service.ts
@@ -5,5 +5,8 @@ export const addPostService = async (
   post: AddPost,
   author: string
 ): Promise<APIResponse> => {
+  if (!Object.keys(post).length) {
+    return { status: 400, message: "Enter post details" };
+  }
   return { status: 201, message: "Post created successfully" };
 };
--- src/tests/post/addPost.test.ts
+++ src/tests/post/addPost.test.ts
@@ -31,5 +31,18 @@ describe("Add Posts", () => {
 
       expect(response.status).toBe(201);
     });
+
+    test("should return 400 if post data is not provided", async () => {
+      const postData = {};
+      const response = await supertest(app)
+        .post("/api/posts")
+        .set(
+          "Authorization",
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MDU1YjM1ZjNiMDdkM2RlY2FiNTQ3YyIsImZpcnN0X25hbWUiOiJUZXN0IiwibGFzdF9uYW1lIjoiVXNlciIsImVtYWlsIjoidGVzdC51c2VyQGplc3QuY29tIiwiaWF0IjoxNzEyMDQ5MDc3LCJleHAiOjE3MTIwNTI2Nzd9.Nn6cnhqPsweP5adReuz8gDOh5klbgOHOjrgnajpOhWM"
+        )
+        .send(postData);
+
+      expect(response.status).toBe(400);
+    });
   });
 });
